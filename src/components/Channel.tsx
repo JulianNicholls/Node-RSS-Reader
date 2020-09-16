901f61ef515e8588edf7c8d2659bca50a58eaa76
Julian Nicholls <juliannicholls29@gmail.com>
2020-09-16 21:27:44 +0100

    Update Channel
    Re-factor ChannelHeader.
    Sort item,s in reverse chronological order.
    Automatically reload after TTL time.

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -5,14 +5,12 @@ import Item from './Item';
 import SelectFeed from './SelectFeed';
 import humanTime from '../humantime';
 
-interface ChannelProps {
+interface HeaderProps {
   channel: FeedChannel;
   reload: () => void;
 }
 
-const Channel = ({ channel, reload }: ChannelProps) => {
-  const reloadRef = useRef<NodeJS.Timeout | null>(null);
-
+const ChannelHeader = ({ channel, reload }: HeaderProps) => {
   const {
     image,
     title,
@@ -21,37 +19,16 @@ const Channel = ({ channel, reload }: ChannelProps) => {
     lastBuildDate,
     pubDate,
     updated,
-    ttl,
     item,
     entry,
   } = channel;
 
-  let items = item ? [...item] : [...entry];
-
+  const items = item ? item.length : entry.length;
   let dateStr: string | null = null;
 
   if (lastBuildDate) dateStr = lastBuildDate;
   else if (pubDate) dateStr = pubDate;
   else if (updated) dateStr = updated;
-
-  items.sort((a, b) => {
-    if (a.pubDate && b.pubDate) {
-      const left = new Date(a.pubDate).valueOf();
-      const right = new Date(b.pubDate).valueOf();
-
-      return right - left;
-    }
-
-    return 1;
-  });
-
-  useEffect(() => {
-    if (ttl) reloadRef.current = setTimeout(reload, ttl * 60 * 1000);
-
-    return () => {
-      if (reloadRef.current) clearTimeout(reloadRef.current);
-    };
-  }, [reload, ttl]);
 
   if (dateStr) {
     const dateDate = new Date(dateStr);
@@ -74,31 +51,67 @@ const Channel = ({ channel, reload }: ChannelProps) => {
   const makeHtml = (text: string) => ({ __html: text });
 
   return (
-    <>
-      <div className="channel-header">
-        <div className="channel-img">
-          {image && <img src={image.url} alt={image.title} />}
-        </div>
-
-        <div className="channel-info">
-          <h2>{title}</h2>
-          {description !== title && <p>{description}</p>}
-          {copyright && (
-            <p
-              className="copyright"
-              dangerouslySetInnerHTML={makeHtml(linkify(copyright))}
-            />
-          )}
-          {dateStr && <p>Last updated {dateStr}</p>}
-
-          <SelectFeed />
-        </div>
-
-        <div className="channel-info2">
-          <span>{items.length} stories</span>
-          <FiRefreshCw className="big-icon" onClick={reload} />
-        </div>
+    <div className="channel-header">
+      <div className="channel-img">
+        {image && <img src={image.url} alt={image.title} />}
       </div>
+
+      <div className="channel-info">
+        <h2>{title}</h2>
+        {description !== title && <p>{description}</p>}
+        {copyright && (
+          <p
+            className="copyright"
+            dangerouslySetInnerHTML={makeHtml(linkify(copyright))}
+          />
+        )}
+        {dateStr && <p>Last updated {dateStr}</p>}
+
+        <SelectFeed />
+      </div>
+
+      <div className="channel-info2">
+        <span>{items} stories</span>
+        <FiRefreshCw className="big-icon" onClick={reload} />
+      </div>
+    </div>
+  );
+};
+
+interface ChannelProps {
+  channel: FeedChannel;
+  reload: () => void;
+}
+
+const Channel = ({ channel, reload }: ChannelProps) => {
+  const reloadRef = useRef<NodeJS.Timeout | null>(null);
+
+  const { ttl, item, entry } = channel;
+
+  let items = item ? [...item] : [...entry];
+
+  items.sort((a, b) => {
+    if (a.pubDate && b.pubDate) {
+      const left = new Date(a.pubDate).valueOf();
+      const right = new Date(b.pubDate).valueOf();
+
+      return right - left;
+    }
+
+    return 1;
+  });
+
+  useEffect(() => {
+    if (ttl) reloadRef.current = setTimeout(reload, ttl * 60 * 1000);
+
+    return () => {
+      if (reloadRef.current) clearTimeout(reloadRef.current);
+    };
+  }, [reload, ttl]);
+
+  return (
+    <>
+      <ChannelHeader channel={channel} reload={reload} />
 
       <div className="container">
         {items.map((item, idx) => (
